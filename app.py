74a2d57774d3e0721d4f920ddc699d5080f52b26
Morris Montemayor <morris.montemayor@azcalun.com>
2025-05-09 17:03:58 +0800

    cors origin

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://balatkayo.datejournals.com", "http://balatkayo.datejournals.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
